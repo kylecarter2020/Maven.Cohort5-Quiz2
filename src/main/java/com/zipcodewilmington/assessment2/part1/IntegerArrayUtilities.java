23c83ce162d13077f9ea435305e1921d4d057c12
Kyle Carter <kylecarter@zipcoders-MacBook-Pro-9.local>
2019-02-27 14:12:29 -0500

    array utilities implemented, most tests working

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -1,19 +1,42 @@
 package com.zipcodewilmington.assessment2.part1;
 
+import java.util.ArrayList;
+
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
         return (array.length % 2) == 0;
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+        ArrayList<Integer> getRange = new ArrayList<>();
+
+        for (int i = start; i <= stop; i++) {
+            getRange.add(i);
+        }
+
+        if(getRange.size() > 0) {
+            Integer[] result = getRange.toArray(new Integer[getRange.size()]);
+            return result;
+        }
+        else {
+            Integer[] result = {};
+            return result;
+        }
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+        Integer sum = 0;
+        for (int i = 0; i < 2; i++) {
+            sum += array[i];
+        }
+        return sum;
     }
 
     public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+        Integer product = 1;
+        for (int i = 0; i < 2; i++) {
+            product *= array[i];
+        }
+        return product;
     }
 }
